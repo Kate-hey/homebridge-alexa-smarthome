9c42ff22983d9663b1986af17fdd92da87bf6fa1
katethefox <katep@hey.com>
2026-04-13 16:13:32 -0500

    Use group name in direction voice command

--- src/accessory/fan-accessory.ts
+++ src/accessory/fan-accessory.ts
@@ -365,7 +365,7 @@ export default class FanAccessory extends BaseAccessory {
     this.lastDirectionSet = now;
     const direction = exhaust ? 'exhaust' : 'direct';
     this.logWithContext('debug', `Triggered set direction: ${direction}`);
-    const command = `set the ${this.device.displayName} to ${direction} mode`;
+    const command = `set the office ${this.device.displayName} to ${direction} mode`;
     return pipe(
       this.platform.alexaApi.sendTextCommand(command),
       TE.match(
